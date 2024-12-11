20a99dac0c258c36dce14ede43b4d0fc82d66052
Noname6022 <nname6034@gmail.com>
2024-12-11 16:17:24 +0400

    Some chagnes

--- src/Pages/Contacts/Contacts.js
+++ src/Pages/Contacts/Contacts.js
@@ -1,113 +1,125 @@
-import React, {useState} from 'react';
-import GlitchText from 'react-glitch-effect/core/GlitchText';
-import TgIcon from '../../media/Telegram.svg';
-import MailIcon from '../../media/Mail.svg';
-import './ContactsStyles.css';
+import React, { useState } from "react";
+import GlitchText from "react-glitch-effect/core/GlitchText";
+import TgIcon from "../../media/Telegram.svg";
+import MailIcon from "../../media/Mail.svg";
+import "./ContactsStyles.css";
 
 export const Contacts = () => {
   const [isDisabled, setDisabled] = useState(false);
   return (
-    <section className='ContactsWrapper panel' id="Pan">
-
-     {/* marquee*/}
-     <div className='TitleContactsContainer'>
-    <div class="marquee">
-  <div class="track">
-    <div class="content">&nbsp;contacts contacts contacts contacts 
-    contacts contacts contacts contacts contacts contacts contacts 
-    contacts contacts contacts contacts contacts contacts contacts 
-    contacts contacts contacts contacts contacts contacts contacts
-     contacts contacts contacts contacts contacts contacts contacts 
-     contacts contacts contacts contacts contacts contacts contacts
-      contacts contacts contacts contacts contacts contacts contacts
-       contacts contacts contacts contacts contacts contacts contacts
-        contacts</div>
-  </div>
-</div>
-    </div>
-    {/* marquee end*/}
-    <div className='ContactsContainer'>
-      <div className='infoContacts'>
-
-      <div class="arrow-wrapper">
-  <div class="arrow"></div>
-  <div class="arrow"></div>
-  <div class="arrow"></div>
-</div>
-      <GlitchText duration="3820"
-            component='h1'  color1="#4CA8E5" color2="#E82167"
-            disabled={isDisabled}>
-           <h1>
-                Interested <br/>
-                to <br/>
-                collaborate <br/>
-                Together?<br/>
-            </h1>
-         </GlitchText>
-         <div class="arrow-wrapper">
-  <div class="arrow"></div>
-  <div class="arrow"></div>
-  <div class="arrow"></div>
-</div>
-           </div>
-
-           <div className='TalkWrapper'>
-             <div classname='TalkContainer'>
-                 <div className='WhiteBox'></div>
-                     <div className='BlackBox'>
-                      <h3>Let's talk</h3>
-                          <div class="arrow-wrapper talkArrowWrapper">
-                              <div class="arrow talkArrow"></div>
-                              <div class="arrow talkArrow"></div>
-                              <div class="arrow talkArrow"></div>
-                            </div>
-                            <div class="wave-text TalkWave">
-                                            TALK TALK TALK TALK
-                                            <br/>TALK TALK TALK TALK
-                                            <br/>TALK TALK TALK TALK
-                                            <br/>TALK TALK TALK TALK
-                                            <br/>TALK TALK TALK TALK
-                                            <br/>TALK TALK TALK TALK
-                                            <br/>TALK TALK TALK TALK
-                                            <br/>TALK TALK TALK TALK
-                                </div>
-                            <div class="arrow-wrapper talkArrowWrapper talkArrowWrapperSecond">
-                              <div class="arrow talkArrow"></div>
-                              <div class="arrow talkArrow"></div>
-                              <div class="arrow talkArrow"></div>
-                            </div>
-
-
-
-                                <a className="telegramLink" href="https://t.me/yopblip" target="_blank"><img className="telegramIcon" src={TgIcon}/></a>
-                                <a className="discordLink"  href = "mailto: ayanami10009@gmail.com"><img className="discordIcon" src={MailIcon}/></a>
-
-              </div>
+    <section className="ContactsWrapper panel" id="Pan">
+      {/* marquee*/}
+      <div className="TitleContactsContainer">
+        <div class="marquee">
+          <div class="track">
+            <div class="content">
+              &nbsp;contacts contacts contacts contacts contacts contacts
+              contacts contacts contacts contacts contacts contacts contacts
+              contacts contacts contacts contacts contacts contacts contacts
+              contacts contacts contacts contacts contacts contacts contacts
+              contacts contacts contacts contacts contacts contacts contacts
+              contacts contacts contacts contacts contacts contacts contacts
+              contacts contacts contacts contacts contacts contacts contacts
+              contacts contacts contacts contacts contacts contacts
             </div>
           </div>
         </div>
+      </div>
+      {/* marquee end*/}
+      <div className="ContactsContainer">
+        <div className="infoContacts">
+          <div class="arrow-wrapper">
+            <div class="arrow"></div>
+            <div class="arrow"></div>
+            <div class="arrow"></div>
+          </div>
+          <GlitchText
+            duration="3820"
+            component="h1"
+            color1="#4CA8E5"
+            color2="#E82167"
+            disabled={isDisabled}>
+            <h1>
+              Interested <br />
+              to <br />
+              collaborate <br />
+              Together?
+              <br />
+            </h1>
+          </GlitchText>
+          <div class="arrow-wrapper">
+            <div class="arrow"></div>
+            <div class="arrow"></div>
+            <div class="arrow"></div>
+          </div>
+        </div>
 
+        <div className="TalkWrapper">
+          <div classname="TalkContainer">
+            <div className="WhiteBox"></div>
+            <div className="BlackBox">
+              <h3>Let's talk</h3>
+              <div class="arrow-wrapper talkArrowWrapper">
+                <div class="arrow talkArrow"></div>
+                <div class="arrow talkArrow"></div>
+                <div class="arrow talkArrow"></div>
+              </div>
+              <div class="wave-text TalkWave">
+                TALK TALK TALK TALK
+                <br />
+                TALK TALK TALK TALK
+                <br />
+                TALK TALK TALK TALK
+                <br />
+                TALK TALK TALK TALK
+                <br />
+                TALK TALK TALK TALK
+                <br />
+                TALK TALK TALK TALK
+                <br />
+                TALK TALK TALK TALK
+                <br />
+                TALK TALK TALK TALK
+              </div>
+              <div class="arrow-wrapper talkArrowWrapper talkArrowWrapperSecond">
+                <div class="arrow talkArrow"></div>
+                <div class="arrow talkArrow"></div>
+                <div class="arrow talkArrow"></div>
+              </div>
+
+              <a
+                className="telegramLink"
+                href="https://t.me/mikii249"
+                target="_blank">
+                <img className="telegramIcon" src={TgIcon} />
+              </a>
+              <a className="discordLink" href="mailto: ayanami10009@gmail.com">
+                <img className="discordIcon" src={MailIcon} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
 
       {/* marquee*/}
-      <div className='TitleContactsContainer'>
-    <div class="marquee">
-  <div class="track">
-    <div class="content">&nbsp;contacts contacts contacts contacts 
-    contacts contacts contacts contacts contacts contacts contacts 
-    contacts contacts contacts contacts contacts contacts contacts 
-    contacts contacts contacts contacts contacts contacts contacts
-     contacts contacts contacts contacts contacts contacts contacts 
-     contacts contacts contacts contacts contacts contacts contacts
-      contacts contacts contacts contacts contacts contacts contacts
-       contacts contacts contacts contacts contacts contacts contacts
-        contacts</div>
-  </div>
-</div>
-    </div>
-    {/* marquee end*/}
-    
-
-</section >
-  )
-}
+      <div className="TitleContactsContainer">
+        <div class="marquee">
+          <div class="track">
+            <div class="content">
+              &nbsp;contacts contacts contacts contacts contacts contacts
+              contacts contacts contacts contacts contacts contacts contacts
+              contacts contacts contacts contacts contacts contacts contacts
+              contacts contacts contacts contacts contacts contacts contacts
+              contacts contacts contacts contacts contacts contacts contacts
+              contacts contacts contacts contacts contacts contacts contacts
+              contacts contacts contacts contacts contacts contacts contacts
+              contacts contacts contacts contacts contacts contacts
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* marquee end*/}
+    </section>
+  );
+};
 export default Contacts;
--- src/Pages/MyProjects/Slider/Slider.js
+++ src/Pages/MyProjects/Slider/Slider.js
@@ -1,130 +1,169 @@
 import React, { Component, useState } from "react";
 import Slider from "react-slick";
-import './SliderStyles.css';
-import ATM from './ImagesCollection/ATM.webp';
-import ATMMobile from './ImagesCollection/ATMMobile.webp';
-import ARG from './ImagesCollection/ARG.webp';
-import ARGMobile from './ImagesCollection/ARGMobile.webp';
-import VND from './ImagesCollection/VND.webp';
-import VNDMobile from './ImagesCollection/VNDMobile.webp';
-import DE from './ImagesCollection/DE.webp';
-import DEMobile from './ImagesCollection/DEMobile.webp';
-import DEDashoard from './ImagesCollection/DEDashoard.webp';
-import DEDashboardMobile from './ImagesCollection/DEDashboardMobile.webp';
-import RightArrow from './ImagesCollection/ArrowRight.svg';
-import LeftArrow from './ImagesCollection/ArrowLeft.svg';
-import MediaQuery from 'react-responsive'
+import "./SliderStyles.css";
+import ATM from "./ImagesCollection/ATM.webp";
+import ATMMobile from "./ImagesCollection/ATMMobile.webp";
+import ARG from "./ImagesCollection/ARG.webp";
+import ARGMobile from "./ImagesCollection/ARGMobile.webp";
+import VND from "./ImagesCollection/VND.webp";
+import VNDMobile from "./ImagesCollection/VNDMobile.webp";
+import DE from "./ImagesCollection/DE.webp";
+import DEMobile from "./ImagesCollection/DEMobile.webp";
+import DEDashoard from "./ImagesCollection/DEDashoard.webp";
+import DEDashboardMobile from "./ImagesCollection/DEDashboardMobile.webp";
+import Ala from "./ImagesCollection/Ala.webp";
+import AlaMobile from "./ImagesCollection/AlaMobile.webp";
+import RightArrow from "./ImagesCollection/ArrowRight.svg";
+import LeftArrow from "./ImagesCollection/ArrowLeft.svg";
+import MediaQuery from "react-responsive";
 
 function NextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-        <div
-        className='rightArrow'
-        onClick={onClick}
-      ><img src={RightArrow } className='Arrow'/> </div>
-    );
-  }
-  
-  function PrevArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className='LeftArrow'
-        onClick={onClick}
-      ><img src={LeftArrow} className='Arrow'/> </div>
-    );
-  }
+  const { className, style, onClick } = props;
+  return (
+    <div className="rightArrow" onClick={onClick}>
+      <img src={RightArrow} className="Arrow" />{" "}
+    </div>
+  );
+}
+
+function PrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div className="LeftArrow" onClick={onClick}>
+      <img src={LeftArrow} className="Arrow" />{" "}
+    </div>
+  );
+}
 
 export default class SlideShow extends Component {
-    
   render() {
-    
-        const settings = {
-          dots: false,
-          infinite: true,
-          speed: 500,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          nextArrow: <NextArrow />,
-          prevArrow: <PrevArrow />
-        };
-        return (
-          <div className="SliderWrapper">
-            <Slider {...settings}>
-              <div className="Slides">
-              <MediaQuery maxWidth={480}>
-              <a href="https://atmosprotocol.com/"  target="_blank">
-                <img src={ATMMobile} className="SlideImages"/>
-                </a>
-                </MediaQuery>
-              <MediaQuery minWidth={481}>
-              <a href="https://atmosprotocol.com/"  target="_blank">
-                <img src={ATM} className="SlideImages"/>
-                </a>
-                </MediaQuery>
-                <a href="https://atmosprotocol.com/" className="SlideLink" target="_blank">Atmos Protocol</a>
-              </div>
-
-
-              <div className="Slides">
-              <MediaQuery maxWidth={480}>
-              <a href="https://www.argano.io/" target="_blank">
-                <img src={ARGMobile} className="SlideImages"/>
-                </a>
-                </MediaQuery>
-              <MediaQuery minWidth={481}>
-              <a href="https://www.argano.io/" target="_blank">
-                <img src={ARG} className="SlideImages"/>
-                </a>
-                </MediaQuery>
-                <a href="https://www.argano.io/" className="SlideLink" target="_blank">Argano</a>
-              </div>
-
-              <div className="Slides">
-              <MediaQuery maxWidth={480}>
-              <a href="https://vineyarddao.netlify.app/" target="_blank">
-                <img src={VNDMobile} className="SlideImages"/>
-                </a>
-                </MediaQuery>
-              <MediaQuery minWidth={481}>
-              <a href="https://vineyarddao.netlify.app/" target="_blank">
-                <img src={VND} className="SlideImages"/>
-                </a>
-                </MediaQuery>
-                <a href="https://vineyarddao.netlify.app/" className="SlideLink" target="_blank">Vineyard DAO</a>
-              </div>
-
-              <div className="Slides">
-              <MediaQuery maxWidth={480}>
-              <a href="https://dubbledex.com/" target="_blank">
-                <img src={DEMobile} className="SlideImages"/>
-                </a>
-                </MediaQuery>
-              <MediaQuery minWidth={481}>
-              <a href="https://dubbledex.com/" target="_blank">
-                <img src={DE} className="SlideImages"/>
-                </a>
-                </MediaQuery>
-                <a href="https://dubbledex.com/" className="SlideLink" target="_blank">Dubble Exchange</a>
-              </div>
-
-              <div className="Slides">
-              <MediaQuery maxWidth={480}>
-              <a href="https://app.dubbledex.com/" target="_blank">
-                <img src={DEDashboardMobile} className="SlideImages"/>
-                </a>
-                </MediaQuery>
-              <MediaQuery minWidth={481}>
-              <a href="https://app.dubbledex.com/" target="_blank">
-                <img src={DEDashoard} className="SlideImages"/>
-                </a>
-                </MediaQuery>
-                <a href="https://app.dubbledex.com/" className="SlideLink SlideLinklong" target="_blank">Dubble Exchange dashboard</a>
-              </div>
-              
-              
-            </Slider>
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      nextArrow: <NextArrow />,
+      prevArrow: <PrevArrow />,
+    };
+    return (
+      <div className="SliderWrapper">
+        <Slider {...settings}>
+          <div className="Slides">
+            <MediaQuery maxWidth={480}>
+              <a href="https://atmosprotocol.com/" target="_blank">
+                <img src={ATMMobile} className="SlideImages" />
+              </a>
+            </MediaQuery>
+            <MediaQuery minWidth={481}>
+              <a href="https://atmosprotocol.com/" target="_blank">
+                <img src={ATM} className="SlideImages" />
+              </a>
+            </MediaQuery>
+            <a
+              href="https://atmosprotocol.com/"
+              className="SlideLink"
+              target="_blank">
+              Atmos Protocol
+            </a>
           </div>
-        );
-      }
-    }
+
+          <div className="Slides">
+            <MediaQuery maxWidth={480}>
+              <a href="https://www.argano.io/" target="_blank">
+                <img src={ARGMobile} className="SlideImages" />
+              </a>
+            </MediaQuery>
+            <MediaQuery minWidth={481}>
+              <a href="https://www.argano.io/" target="_blank">
+                <img src={ARG} className="SlideImages" />
+              </a>
+            </MediaQuery>
+            <a
+              href="https://www.argano.io/"
+              className="SlideLink"
+              target="_blank">
+              Argano
+            </a>
+          </div>
+
+          <div className="Slides">
+            <MediaQuery maxWidth={480}>
+              <a href="https://vineyarddao.netlify.app/" target="_blank">
+                <img src={VNDMobile} className="SlideImages" />
+              </a>
+            </MediaQuery>
+            <MediaQuery minWidth={481}>
+              <a href="https://vineyarddao.netlify.app/" target="_blank">
+                <img src={VND} className="SlideImages" />
+              </a>
+            </MediaQuery>
+            <a
+              href="https://vineyarddao.netlify.app/"
+              className="SlideLink"
+              target="_blank">
+              Vineyard DAO
+            </a>
+          </div>
+
+          <div className="Slides">
+            <MediaQuery maxWidth={480}>
+              <a href="https://dubbledex.com/" target="_blank">
+                <img src={DEMobile} className="SlideImages" />
+              </a>
+            </MediaQuery>
+            <MediaQuery minWidth={481}>
+              <a href="https://dubbledex.com/" target="_blank">
+                <img src={DE} className="SlideImages" />
+              </a>
+            </MediaQuery>
+            <a
+              href="https://dubbledex.com/"
+              className="SlideLink"
+              target="_blank">
+              Dubble Exchange
+            </a>
+          </div>
+
+          <div className="Slides">
+            <MediaQuery maxWidth={480}>
+              <a href="https://app.dubbledex.com/" target="_blank">
+                <img src={DEDashboardMobile} className="SlideImages" />
+              </a>
+            </MediaQuery>
+            <MediaQuery minWidth={481}>
+              <a href="https://app.dubbledex.com/" target="_blank">
+                <img src={DEDashoard} className="SlideImages" />
+              </a>
+            </MediaQuery>
+            <a
+              href="https://app.dubbledex.com/"
+              className="SlideLink SlideLinklong"
+              target="_blank">
+              Dubble Exchange dashboard
+            </a>
+          </div>
+
+          <div className="Slides">
+            <MediaQuery maxWidth={480}>
+              <a href="https://a-lmd.com/" target="_blank">
+                <img src={AlaMobile} className="SlideImages" />
+              </a>
+            </MediaQuery>
+            <MediaQuery minWidth={481}>
+              <a href="https://a-lmd.com/" target="_blank">
+                <img src={Ala} className="SlideImages" />
+              </a>
+            </MediaQuery>
+            <a
+              href="https://a-lmd.com/"
+              className="SlideLink SlideLinklong"
+              target="_blank">
+              Alameda Construction
+            </a>
+          </div>
+        </Slider>
+      </div>
+    );
+  }
+}
